7bffd9eff878e01b68a7cfee8f1a7dec63dd6b44
vinny17690 <vinay.vinny18@gmail.com>
2022-08-20 12:49:59 -0400

    1 file modified

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // Import stylesheets
 import './style.css';
 
+const log = (msg) => console.log(msg);
+
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>Design Patterns</h1>`;
 
-console.log('/// Factory Pattern ///')
+log('/// Factory Pattern ///');
 
 // Factory Function
 function Vehicle(make, year) {
@@ -23,20 +25,20 @@ function Vehicle(make, year) {
 }
 
 const hondaInfo = new Vehicle('Honda', 'Accord');
-console.log(hondaInfo);
-console.log(hondaInfo.makeInfo);
+log(hondaInfo);
+log(hondaInfo.makeInfo);
 
 // Singleton Pattern
 // Problem: Assume we have a database for an application. We need a single database connection
 // across the application. So, we need to find a way to store this database connection after it got initialized once. This is where Single Pattern helps. It keeps the reference to the initialized object.
 
-console.log('/// Singleton Pattern ///')
+log('/// Singleton Pattern ///');
 
 const db = (function () {
   function Database(username, password) {
     this.username = username;
     this.password = password;
-    console.log('Connection Initialized!');
+    log('Connection Initialized!');
   }
 
   let dbInstance = null;
@@ -45,7 +47,7 @@ const db = (function () {
     getInstance: function () {
       if (!dbInstance) {
         dbInstance = new Database('user', 'pass');
-        dbInstance.constructor = null // Set constructor to null after storing instance in a variable
+        dbInstance.constructor = null; // Set constructor to null after storing instance in a variable
       }
       return dbInstance;
     },
@@ -55,11 +57,47 @@ const db = (function () {
 const dbConnection = db.getInstance();
 const dbConnection1 = db.getInstance();
 const dbConnection2 = db.getInstance();
-console.log(dbConnection);
-console.log(dbConnection1);
-console.log(dbConnection2); // We see only one time database connection was initialized. That's singleton!
+log(dbConnection);
+log(dbConnection1);
+log(dbConnection2); // We see only one time database connection was initialized. That's singleton!
 
 // Strategy Pattern
-console.log('/// Strategy Pattern ///')
+log('/// Strategy Pattern ///');
+/**
+ * Problem: Get quote from various insurance providers using Strategy pattern
+ */
 
+function TDInsurance(quote) {
+  this.provider = 'TD Insurance';
+  this.getQuote = () => 80;
+}
+function ScotiaInsurance(quote) {
+  this.provider = 'Scotia Insurance';
+  this.getQuote = () => 75;
+}
+function RBCInsurance(quote) {
+  this.provider = 'RBC Insurance';
+  this.getQuote = () => 88;
+}
 
+const td = new TDInsurance();
+const scotia = new ScotiaInsurance();
+const rbc = new RBCInsurance();
+
+// Build a master strategry class to be able to call insurance provider based on choice/strategy
+function InsuranceStrategy() {
+  this.insuranceProvider = null;
+  this.setStrategry = (provider) => {
+    this.insuranceProvider = provider;
+  };
+}
+
+// InsuranceStrategy will have setStrategy to be able to set the insurance provider of choice
+// Since all providers implement getQuote method, it's easy to
+const strategy = new InsuranceStrategy();
+log(strategy);
+// For example, if we want to get quote from TD, then set the strategy to TD, and get quotation by calling getQuote()
+strategy.setStrategry(td);
+log(strategy.insuranceProvider.getQuote());
+
+log('/// Proxy Pattern ///');
